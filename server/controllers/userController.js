95edc05adb317dffe58119d9af27df1ed61d38de
Ura427 <urakost427@gmail.com>
2024-02-23 00:32:19 +0200

    add functionality to methods

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,19 @@
+const ApiError = require("../error/ApiError");
+
 class UserController {
   async registration(req, res) {}
 
-  async login(req, res) {}
+  async login(req, res) {
+    res.status(200).json({message: "Login Works"})
+  }
 
-  async auth(req, res) {}
+  async check(req, res) {
+    const { id } = req.query;
+    if (!id) {
+      return next(ApiError.badRequest());
+    }
+    res.json(id);
+  }
 }
 
 module.exports = new UserController();

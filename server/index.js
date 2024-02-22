86fe54154ed06e2a8de970083f975c5c1eb19d31
Ura427 <urakost427@gmail.com>
2024-02-23 00:28:43 +0200

    add and use new modules

--- server/index.js
+++ server/index.js
@@ -2,12 +2,22 @@ require("dotenv").config();
 const express = require("express");
 const sequelize = require("./db");
 const models = require("./models/models");
-const cors= require("cors")
+const cors = require("cors");
+const router = require("./routes/index");
+const fileUpload = require("express-fileupload");
+const errorHandler = require("./middleware/ErrorHandlingMiddleware");
+const path = require("path");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "static")));
+app.use(fileUpload({}));
+app.use("/api", router);
+
+//Error handler THE LAST MIDDLEWARE
+app.use(errorHandler);
 
 const start = async () => {
   try {

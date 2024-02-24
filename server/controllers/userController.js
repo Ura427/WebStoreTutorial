f86d9054e3b25fbe86b6c8d77193750c6c96c669
Ura427 <urakost427@gmail.com>
2024-02-24 21:57:58 +0200

    add functionality to methods

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,18 +1,57 @@
 const ApiError = require("../error/ApiError");
+const bcrypt = require("bcrypt");
+const { User, Basket } = require("../models/models");
+const jwt = require("jsonwebtoken");
+
+const generateJwt = (id, email, role) => {
+  return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
+    expiresIn: "24h",
+  });
+};
 
 class UserController {
-  async registration(req, res) {}
+  async registration(req, res, next) {
+    const { email, password, role } = req.body;
 
-  async login(req, res) {
-    res.status(200).json({message: "Login Works"})
+    if (!email || !password) {
+      return next(ApiError.badRequest("Email or password is abscent"));
+    }
+
+    const candidate = await User.findOne({ where: { email } });
+
+    if (candidate) {
+      return next(ApiError.badRequest("User with this email already exist"));
+    }
+
+    const hashPassword = await bcrypt.hash(password, 5);
+    const user = await User.create({ email, role, password: hashPassword });
+    const basket = await Basket.create({ userId: user.id });
+
+    const token = generateJwt(user.id, email, role);
+
+    return res.json({ token });
+  }
+
+  async login(req, res, next) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return next(ApiError.internal("User not found"));
+    }
+
+    const comparedPasswords = bcrypt.compareSync(password, user.password);
+    if (!comparedPasswords) {
+      return next(ApiError.internal("Invalid password"));
+    }
+    const token = generateJwt(user.id, email, user.role);
+    return res.json({ token });
   }
 
   async check(req, res) {
-    const { id } = req.query;
-    if (!id) {
-      return next(ApiError.badRequest());
-    }
-    res.json(id);
+    const token = generateJwt(req.user.id, req.user.email, req.user.role);
+    return res.json({ token });
   }
 }
 

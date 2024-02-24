b605d166a9b199fbc97bcc10bb50d122ffff2f27
Ura427 <urakost427@gmail.com>
2024-02-24 21:56:13 +0200

    remove test route, add auth middleware

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,10 +1,10 @@
 const Router = require("express");
 const router = new Router();
 const userController = require("../controllers/userController");
+const authMiddleware = require("../middleware/authMiddleware")
 
 router.post("/registration", userController.registration);
 router.post("/login", userController.login);
-router.get("/auth", userController.check);
-router.use("/test", (req,res) => {res.status(200).json({message: "Works"})})
+router.get("/auth", authMiddleware, userController.check);
 
 module.exports = router;
